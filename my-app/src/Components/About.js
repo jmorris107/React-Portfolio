ee1cf5b658e134bbec6f1b1811e2e2c6f2795412
Jennifer Morris <jenniferinbaranoseidou@gmail.com>
2021-01-04 16:39:07 -0500

    fixed images link

--- my-app/src/Components/About.js
+++ my-app/src/Components/About.js
@@ -20,7 +20,7 @@ class About extends Component {
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Tim Baker Profile Pic" />
+            <img className="profile-pic"  src={profilepic} alt="Jennifer Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -38,11 +38,7 @@ class About extends Component {
                      <span>{email}</span>
 					   </p>
                </div>
-               <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div>
+               
             </div>
          </div>
       </div>
